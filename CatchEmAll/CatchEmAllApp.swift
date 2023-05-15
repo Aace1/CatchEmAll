f0bd8dd22def38c4e91a1326abc9c3fe3df205b1
Aaron Anderson <aace2@me.com>
2023-05-15 09:10:25 -0700

    Initial Commit

---
+++ CatchEmAll/CatchEmAllApp.swift
@@ -0,0 +1,17 @@
+//
+//  CatchEmAllApp.swift
+//  CatchEmAll
+//
+//  Created by TheForce on 5/15/23.
+//
+
+import SwiftUI
+
+@main
+struct CatchEmAllApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
